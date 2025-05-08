b02836c321a3fad2007b4752d420297a55f23500
Wouter Wijsman <wwijsman@live.nl>
2025-05-08 15:49:10 +0200

    Fix centering of loading screen

--- src/LoadScreen.cpp
+++ src/LoadScreen.cpp
@@ -13,6 +13,9 @@ void LoadScreen::run(Window * window, Config * config, ResourceManager * resourc
   std::atomic<int> progress;
   std::atomic<bool> is_done = false;
   SDL_Color loading_bar_color = config->getProgressColor();
+  SDL_Rect background_rect;
+  SDL_QueryTexture(background, NULL, NULL, &background_rect.w, &background_rect.h);
+  SDL_Rect window_rect;
   SDL_Rect loading_bar_rect = config->getProgressPosition();
 
   std::thread loading_thread(&ResourceManager::load_all, resource_manager, &progress, &is_done);
@@ -25,9 +28,14 @@ void LoadScreen::run(Window * window, Config * config, ResourceManager * resourc
           break;
       }
     }
-    SDL_RenderCopy(window->renderer, background, NULL, NULL);
+    SDL_GetWindowSize(window->window, &window_rect.w, &window_rect.h);
+    background_rect.x = window_rect.w /2 - background_rect.w / 2;
+    background_rect.y = window_rect.h /2 - background_rect.h / 2;
+    SDL_RenderCopy(window->renderer, background, NULL, &background_rect);
 
     SDL_Rect loading_bar_draw_rect = loading_bar_rect;
+    loading_bar_draw_rect.x += background_rect.x;
+    loading_bar_draw_rect.y += background_rect.y;
     loading_bar_draw_rect.w = (int) ((float) loading_bar_draw_rect.w / 100.0f * (float) progress);
     SDL_SetRenderDrawColor(window->renderer, loading_bar_color.r, loading_bar_color.g, loading_bar_color.b, 255);
     SDL_RenderFillRect(window->renderer, &loading_bar_draw_rect);
