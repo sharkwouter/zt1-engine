49a1aa7c9a2f49abe77794a214bca4c8f947fe3d
Wouter Wijsman <wwijsman@live.nl>
2025-05-08 23:19:31 +0200

    Set layer to 0 by default

--- src/ui/UiElement.hpp
+++ src/ui/UiElement.hpp
@@ -53,7 +53,7 @@ protected:
   ResourceManager * resource_manager = nullptr;
   std::string name;
   int id = 0;
-  int layer;
+  int layer = 0;
   int anchor = 0;
 
   std::vector<UiElement*> children;
