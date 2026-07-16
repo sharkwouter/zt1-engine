ee3fb1064870abd2a21882c5537567529fffe440
Wouter Wijsman <wwijsman@live.nl>
2026-07-16 22:30:49 +0200

    Load fonts from directory the zt1-engine executable is in

--- src/FontManager.cpp
+++ src/FontManager.cpp
@@ -72,5 +72,5 @@ void FontManager::loadFont(const int font) {
       break;
   }
 
-  this->fonts[font] = TTF_OpenFont(Utils::fixPath("font/" + font_file).c_str(), font_size);
+  this->fonts[font] = TTF_OpenFont((Utils::getExecutableDirectory() + "/font/" + font_file).c_str(), font_size);
 }
